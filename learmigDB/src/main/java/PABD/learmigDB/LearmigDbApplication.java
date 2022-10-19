9e0f58c666d756b9826f9c5b0c75f6dc152abf7a
ryande55 <101327364+ryande55@users.noreply.github.com>
2022-10-19 20:43:17 +0700

    Add files via upload

---
+++ learmigDB/src/main/java/PABD/learmigDB/LearmigDbApplication.java
@@ -0,0 +1,13 @@
+package PABD.learmigDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearmigDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearmigDbApplication.class, args);
+	}
+
+}
---
+++ learmigDB/src/test/java/PABD/learmigDB/LearmigDbApplicationTests.java
@@ -0,0 +1,13 @@
+package PABD.learmigDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearmigDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
